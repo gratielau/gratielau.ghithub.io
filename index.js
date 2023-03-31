3114c07743e4cc164d66b2f1f6f09a3b26f43af3
Gratiela Uivari <gratiela.uivari@indsoft.ro>
2023-03-31 19:16:18 +0300

    some css

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var activePage = "home";
+setPaginaActiva("home");
 
-var homePage = document.getElementById(activePage);
-homePage.style.display = "block";
+function changePage(activePage) {
+  setPaginaInactiva("home");
+  setPaginaInactiva("skills");
+  setPaginaInactiva("projects");
+  setPaginaInactiva("languages");
+  setPaginaActiva(activePage);
+}
 
-function showSkills() {
-  console.warn("te rog afiseaza skills");
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "block";
+function setPaginaInactiva(page) {
+  document.getElementById(page).style.display = "none";
+  var el = document.getElementById("a-" + page);
+  el.style.backgroundColor = "#00897b";
+  el.style.color = "#fff";
+}
+
+function setPaginaActiva(page) {
+  document.getElementById(page).style.display = "block";
+  var el = document.getElementById("a-" + page);
+  el.style.color = "#00897b";
+  el.style.backgroundColor = "#fff";
+  document.getElementById(page);
 }
